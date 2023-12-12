25656c74d494cc2479ea68c0860c62d05c90ae76
Hellbent <hellbent105@gmail.com>
2023-12-12 20:38:20 +0200

    laba4

--- BlogProject/blog/models.py
+++ BlogProject/blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=100)
 
     def __str__(self):
@@ -16,7 +16,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author_name = models.CharField(max_length=255)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f"Comment by {self.author_name} on {self.post.title}"
--- BlogProject/blog/views.py
+++ BlogProject/blog/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from blog.models import Post, Comment
+from blog.serializers import PostSerializer, CommentSerializer
+from rest_framework.generics import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
 
-# Create your views here.
+class PostList(APIView):
+    @swagger_auto_schema(responses={200: PostSerializer(many=True)})
+    def get(self, request, *args, **kwargs):
+        module1_objects = Post.objects.all()
+        serializer = PostSerializer(module1_objects, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(responses={200: PostSerializer(many=True)})
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    def get(self, request, module1_id, *args, **kwargs):
+        module1_object = get_object_or_404(Post, id=module1_id)
+        serializer = PostSerializer(module1_object)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, module1_id, *args, **kwargs):
+        module1_object = get_object_or_404(Post, id=module1_id)
+        serializer = PostSerializer(module1_object, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, module1_id, *args, **kwargs):
+        module1_object = get_object_or_404(Post, id=module1_id)
+        module1_object.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentList(APIView):
+    @swagger_auto_schema(responses={200: CommentSerializer(many=True)})
+    def get(self, request, *args, **kwargs):
+        module2_objects = Comment.objects.all()
+        serializer = CommentSerializer(module2_objects, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(responses={200: CommentSerializer(many=True)})
+    def post(self, request, *args, **kwargs):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetail(APIView):
+    def get(self, request, module1_id, *args, **kwargs):
+        module1_object = get_object_or_404(Comment, id=module1_id)
+        serializer = CommentSerializer(module1_object)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, module1_id, *args, **kwargs):
+        module1_object = get_object_or_404(Comment, id=module1_id)
+        serializer = CommentSerializer(module1_object, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, module1_id, *args, **kwargs):
+        module1_object = get_object_or_404(Comment, id=module1_id)
+        module1_object.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
